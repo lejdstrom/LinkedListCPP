8e8d76dc599ba56bb604d544e4fd119a650a2f75
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-07-09 12:12:20 +0300

    LinkedList is now cpp file Skeleton of class

---
+++ LinkedList.cpp
@@ -0,0 +1,10 @@
+#include "LinkedList.h"
+
+
+LinkedList::LinkedList(/* args */)
+{
+}
+
+LinkedList::~LinkedList()
+{
+}
--- LinkedList.h
+++ LinkedList.h
@@ -1,4 +1,22 @@
 #ifndef LINKED_LIST_H
 #define LINDED_LIST_H
 
+struct Link{
+    int value;
+    struct Link * next;
+};
+
+class LinkedList
+{
+public:
+    Link head;
+    LinkedList(/* args */);
+    ~LinkedList();
+};
+
+
+
+
+
+
 #endif
